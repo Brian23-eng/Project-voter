31e8692b27ae3edb01e97156135530480d5c825c
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-22 22:49:23 +0300

    add authentication for user and create registration and login form'

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('voter.urls'))
+    url(r'', include('voter.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
