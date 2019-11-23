7666e56256c2d186e849d64af6d0d6a29c03d351
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-23 14:32:54 +0300

    create a function that use the post methods to post user input

--- voter/views.py
+++ voter/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Profile, Post, Rating
+from . forms import PostForm, UpdateUserProfileForm,UpdateUserForm, RatingsForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+import random   
+
 
 def home(request):
-    return render(request,'home.html')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+    else:
+        form = PostForm()
+        
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, 1(posts(-1)))
+        random_post = posts[a_post]
+        print(random_post.photo)
+        
+    except Post.DoesNotExist:
+        posts = None
+            
+    return render(request,'home.html', {'posts':posts, 'forms':form, 'random_post':random_post})
