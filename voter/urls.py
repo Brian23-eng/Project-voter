31e1779fde8f5fd3e41fbe4b8c341158912d1b45
brian23-eng <b.odhiambo.bo@gmail.com>
2019-11-23 17:29:03 +0300

    add route for each and every function created in views

--- voter/urls.py
+++ voter/urls.py
@@ -3,12 +3,22 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+
+
+
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('posts', views.PostViewSet)
 router.register('profile', views.ProfileViewSet)
 
 urlpatterns=[
-    url('^$', views.home, name='home')
+    url('^$', views.home, name='home'),
+    url(r'^api/', include(router.urls)),
+    url(r'^<username>/profile', views.user_profile, name='userprofile'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^profile/<username>/', views.profile, name='profile'),
+    url(r'^profile/<username>/settings', views.edit_profile, name='edit'),
+    url(r'^project/<post>', views.project, name='project'),
+    url(r'^search/', views.search_project, name='search'),
 ]
 
